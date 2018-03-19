dbe1994d404d009c781705c92dd61445020cc755
RKragh <36151118+RKragh@users.noreply.github.com>
2018-03-19 10:49:21 +0100

    Pictures

--- ProjectRasmusProgIII/Models/Product.cs
+++ ProjectRasmusProgIII/Models/Product.cs
@@ -13,9 +13,14 @@ namespace ProjectRasmusProgIII.Models
         [Display(Name = "Product Name")]
         public string ProductName { get; set; }
         [Required]
-        [Display(Name = "Kategori")]
+        [Display(Name = "Category")]
         public string ProductCategory { get; set; }
         [Required]
+        [Display(Name = "Price")]
+        public double ProductPrice { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "path too long"), MinLength(1)]
+        public string ProductImagePath { get; set; }
 
     }
 }
