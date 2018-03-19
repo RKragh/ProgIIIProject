7d8d5e46c3a9356a110c68ebd0b5bcfed6c8064e
RKragh <36151118+RKragh@users.noreply.github.com>
2018-03-19 08:58:20 +0100

    First Commit

---
+++ ProjectRasmusProgIII/Models/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectRasmusProgIII.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "This field can not be longer than 50 characters"), MinLength(1)]
+        public string CustomerFirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "This field can not be longer than 50 characters"), MinLength(1)]
+        public string CustomerLastName { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public int CustomerPhone { get; set; }
+        [Required]
+        [Display(Name = "Address")]
+        public string CustomerAddress { get; set; }
+        [Required]
+        [Display(Name = "City")]
+        public string CustomerCity { get; set; }
+        [Required]
+        [Display(Name = "Zip")]
+        public int CustomerZipCode { get; set; }
+        [Required]
+        [Display(Name = "Country")]
+        public string CustomerCountry { get; set; }
+
+        public string GetFullAddress()
+        {
+            string address = "";
+
+            address = CustomerAddress + "\n" +
+                      CustomerZipCode + ", " + CustomerCity + "\n" +
+                      CustomerCountry;
+
+            return address;
+        }
+    }
+}
---
+++ ProjectRasmusProgIII/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectRasmusProgIII.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        [Required]
+        [Display(Name = "Kategori")]
+        public string ProductCategory { get; set; }
+        [Required]
+
+    }
+}
